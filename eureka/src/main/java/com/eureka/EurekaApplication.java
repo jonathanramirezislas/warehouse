6bca73d25662dac58d07d4d7fd5ea58624b2791d
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2021-04-01 20:14:37 -0600

    set up

--- eureka/src/main/java/com/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/eureka/EurekaApplication.java
@@ -1,13 +1,18 @@
 package com.eureka;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApplication.class, args);
+		System.out.println("Working eureka......");
+
 	}
 
 }
