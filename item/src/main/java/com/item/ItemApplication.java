016f112202ebc0362f1fef7f015fc10a9b3f1a7b
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2021-04-01 23:47:17 -0600

    register microservices in eureka

--- item/src/main/java/com/item/ItemApplication.java
+++ item/src/main/java/com/item/ItemApplication.java
@@ -2,10 +2,12 @@ package com.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name="servicio-productos") //only one client using Feign (folder cliente)
+
+//@RibbonClient(name="servicio-productos") //only one client using Feign (folder cliente)
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class ItemApplication {
