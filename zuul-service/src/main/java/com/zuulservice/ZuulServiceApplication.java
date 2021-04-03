ae43b4235214f95a12b6f653e415194e2a120bb5
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2021-04-02 18:13:46 -0600

    :wrench: increased ribbon timeout

--- zuul-service/src/main/java/com/zuulservice/ZuulServiceApplication.java
+++ zuul-service/src/main/java/com/zuulservice/ZuulServiceApplication.java
@@ -2,6 +2,8 @@ package com.zuulservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableEurekaClient
 @EnableZuulProxy
@@ -10,6 +12,7 @@ public class ZuulServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulServiceApplication.class, args);
+		System.out.println("zuul");
 	}
 
 }
