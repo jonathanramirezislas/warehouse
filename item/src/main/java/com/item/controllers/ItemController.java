75117cc3ca3215663f5327026d57d35d44672834
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2021-04-05 23:17:50 -0500

    config  boostrap and endpoint to read see properties of microservice

--- item/src/main/java/com/item/controllers/ItemController.java
+++ item/src/main/java/com/item/controllers/ItemController.java
@@ -1,23 +1,39 @@
 package com.item.controllers;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.item.models.Item;
 import com.item.models.Producto;
 import com.item.services.IItemService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class ItemController {
 
+    private static Logger log = LoggerFactory.getLogger(ItemController.class);
+
+
+
     @Autowired
     @Qualifier("serviceFeign")
     private IItemService itemService;
+
+    //leyendo propiedades forma 1
+    @Value("${configuracion.texto}")
+    private String texto;
 
     @GetMapping("/listar")
     public List<Item> listar(){
@@ -60,6 +76,19 @@ public class ItemController {
         item.setProducto(producto);
         return item;
 
+    }
+
+    @GetMapping("/obtener-config")          //leyendo propeidades forma 2
+    public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto){
+
+        log.info(texto);
+
+        Map<String, String> json = new HashMap<>();
+        json.put("texto", texto); //otenido de propiedades
+        json.put("puerto", puerto);//obitenido de propiedades
+
+
+        return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
     }
 
 }
