c40f523eada7bd50d7955189467ffe7c888297de
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2021-04-06 18:09:19 -0500

    RefreshScope

--- item/src/main/java/com/item/controllers/ItemController.java
+++ item/src/main/java/com/item/controllers/ItemController.java
@@ -10,6 +10,7 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +22,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@RefreshScope
+//allows us to updated components when properties change but except properties such as port,name microservices ,
 @RestController
 public class ItemController {
 
