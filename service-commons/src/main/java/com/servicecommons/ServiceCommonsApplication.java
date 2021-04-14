cc5f786278d560daf39ba8847c81f67d94d02d3b
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2021-04-13 22:31:37 -0500

    :tada: commons service

---
+++ service-commons/src/main/java/com/servicecommons/ServiceCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.servicecommons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceCommonsApplication.class, args);
+	}
+
+}
---
+++ service-commons/src/test/java/com/servicecommons/ServiceCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicecommons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
