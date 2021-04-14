cce0318874628e200bbd3be1ed1d40c020923bd0
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2021-04-14 18:08:08 -0500

    :wrench: delete dependency from h2 by excluding from spring's configuration

--- commons-service/src/main/java/com/commonsservice/CommonsServiceApplication.java
+++ commons-service/src/main/java/com/commonsservice/CommonsServiceApplication.java
@@ -1,13 +1,12 @@
 package com.commonsservice;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class CommonsServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CommonsServiceApplication.class, args);
-	}
 
 }
