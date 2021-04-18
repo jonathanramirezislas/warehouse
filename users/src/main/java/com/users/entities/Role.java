339b3473acc7e32672ab99c890eed45f29e97f23
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2021-04-17 23:11:57 -0500

    :construction: entites Role & User

---
+++ users/src/main/java/com/users/entities/Role.java
@@ -0,0 +1,42 @@
+package com.users.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "role")
+public class Role implements Serializable {
+
+    private static final long serialVersionUID = 4467531611801172710L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, length = 30)
+    private String nombre;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+}
